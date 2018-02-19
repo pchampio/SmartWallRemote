c4f21912159d21765ef19adac6188cf175f8877e
Hactogeek <tony.marteau@outlook.com>
2018-02-19 14:38:42 +0100

    Ajout de Scenario

---
+++ app/src/main/java/sdw/drakirus/xyz/smartwallremote/component/scenario/ScenarioChooserAdapter.java
@@ -0,0 +1,71 @@
+package sdw.drakirus.xyz.smartwallremote.component.scenario;
+
+/**
+ * Created by drakirus (p.champion) on 26/01/18.
+ */
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import sdw.drakirus.xyz.smartwallremote.R;
+import sdw.drakirus.xyz.smartwallremote.json.Layout;
+
+public class ScenarioChooserAdapter extends BaseAdapter {
+
+    private final List<Layout> layoutList;
+    private LayoutInflater layoutInflater;
+
+    public ScenarioChooserAdapter(Context context, List<Layout> dataList) {
+        layoutInflater = LayoutInflater.from(context);
+        this.layoutList = dataList;
+    }
+
+    @Override
+    public int getCount() {
+        return layoutList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ScenarioViewHolder viewHolder;
+        View view = convertView;
+
+        if (view == null) {
+            view = layoutInflater.inflate(R.layout.list_scenario, parent, false);
+            viewHolder = new ScenarioViewHolder();
+            viewHolder.textView = (TextView) view.findViewById(R.id.text_view);
+            viewHolder.imageView = (ImageView) view.findViewById(R.id.image_view);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ScenarioViewHolder) view.getTag();
+        }
+
+        Bitmap bitmap = UtilsScenario.makeBitmap(layoutList.get(position));
+
+        // data bind to viewHolder
+        // viewHolder.imageView.setImageResource(R.drawable.ic_launcher_background);
+        viewHolder.imageView.setImageBitmap(bitmap);
+        viewHolder.textView.setText(layoutList.get(position).getName());
+
+
+        return view;
+    }
+}
---
+++ app/src/main/java/sdw/drakirus/xyz/smartwallremote/component/scenario/UtilsScenario.java
@@ -0,0 +1,47 @@
+package sdw.drakirus.xyz.smartwallremote.component.scenario;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import sdw.drakirus.xyz.smartwallremote.json.GrpScreen;
+import sdw.drakirus.xyz.smartwallremote.json.Layout;
+import sdw.drakirus.xyz.smartwallremote.json.Screen;
+
+/**
+ * Created by Hactogeek on 14/02/2018.
+ */
+
+public class UtilsScenario {
+    public static Bitmap makeBitmap(Layout layout){
+
+        int widthTotal = 125 * layout.getCols();
+        int heightTotal = 75 * layout.getRows();
+
+        Bitmap scenarioImage = Bitmap.createBitmap(widthTotal, heightTotal, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(scenarioImage);
+
+        int width = widthTotal - 5;
+        int height = heightTotal - 5;
+        int widthScreen = (width - layout.getCols()) / layout.getCols();
+        int heightScreen = (height - layout.getRows()) / layout.getRows();
+        Paint paintBackground = new Paint();
+        paintBackground.setColor(Color.GRAY);
+        canvas.drawRect(0F, 0F, (float) width, (float) height, paintBackground);
+
+        Paint paint = new Paint();
+
+        for (GrpScreen grpScreen : layout.getGrpScreen()){
+            paint.setColor(grpScreen.getColor());
+            paint.setAlpha(210);
+
+            for (Screen screen : grpScreen.getListScreen()){
+                canvas.drawRect((float)(widthScreen * screen.getCol()+5),(float)(heightScreen * screen.getRow()+5), (float)(widthScreen * (screen.getCol()+1)), (float)(heightScreen * (screen.getRow()+1)), paint);
+            }
+
+        }
+
+        return scenarioImage;
+    }
+}
